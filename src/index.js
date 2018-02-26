b0430247dc125bdfbe266732933e76e8921c5489
Andrey Sorokin <anderysorokin@gmail.com>
2018-02-27 00:51:09 +0300

    all done!
    fix lasts bags

--- src/index.js
+++ src/index.js
@@ -1,24 +1,23 @@
 module.exports = function longestConsecutiveLength(array) {
   	var tempArr=[];
   	var cont=1;
-   array.sort((l,r)=>l-r);
+      array.sort((l,r)=>l-r);
    for(let i=1;i<array.length;i++){
    		if(array[i]===array[i-1]+1){
    			cont++
    		} else{
         if(array[i]!==array[i-1]){
-            if(cont>1){
+            if(cont>0){
              tempArr.push(cont);
             }
             cont=1;
         } 
-	
    		}
-   		
+ 		
    		
    }
-  return array.length<2? array.length:tempArr.sort((l,r)=>r-l)[0] ;
-
- 
-   
+   if(tempArr.length==0||tempArr.length==1||array.length==1){
+     return array.length==0?0:1;
+   }
+  return tempArr.length<2? tempArr.length:tempArr.sort((l,r)=>r-l)[0] ;
 }
